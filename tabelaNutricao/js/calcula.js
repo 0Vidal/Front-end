793fe07fa2c70614d5068c7a2b33506a0b0ae398
0Vidal <oliveira.vidal.vinicius@escola.pr.gov.br>
2025-10-08 09:46:57 -0300

    Add files via upload

---
+++ tabelaNutricao/js/calcula.js
@@ -0,0 +1,74 @@
+// Foi alterado o título e subtítulo.
+var	tSecundario = document.querySelector(".tsecundario");
+var titulo = document.querySelector(".texto");
+tSecundario.innerText = 'Meus Pacientes';
+titulo.innerText = 'Paulista Nutrições';
+
+// Cria a variável paciente e peso pela classe.
+var pacientes = document.querySelectorAll(".paciente");
+
+for(i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.innerText;
+    
+    // Cria a variável altura pela classe.
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.innerText;
+    
+    // Define a função de cálculo do IMC a varíavel IMC, dentor dos parenteses colocamo os parametros, para utilizar as varias utilizadas dentro da função
+    var IMC = calculaIMC(peso, altura);
+    
+    // Valores booleanos
+    
+    var pesoValido = true;
+    var alturaValida = true;
+    
+    if (pesoValido && alturaValida){
+        var tdIMC = paciente.querySelector(".info-imc");
+        tdIMC.innerText = IMC;
+    }
+
+    if(!validarPeso(peso)){
+        console.log("Peso Inválido!")
+        var pesoValido = false;
+        tdIMC.innerText = 'Peso Inválido!';
+        paciente.classList.add('paciente__invalido');
+    }
+    
+    if (!validarAltura(altura)){
+        var alturaValida = false;
+        //alert("Altura Inválida!");
+        tdIMC.innerText = 'Altura Inválida!';
+        paciente.classList.add('paciente__invalido');
+
+    }
+    
+};      
+
+function calculaIMC (peso, altura){
+    var imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+};
+
+function validarPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+};
+
+function validarAltura(altura){
+    if(altura >= 0.00 && altura < 3.00){
+        return true;
+    }
+    else{
+        return false;
+    }
+};
---
+++ tabelaNutricao/js/formulario.js
@@ -0,0 +1,69 @@
+var addPaciente = document.querySelector('#adicionar-paciente');
+
+addPaciente.addEventListener('click', 
+    function (event){
+        event.preventDefault();
+
+        // seleciona o formulário
+        var form = document.querySelector('#form-adiciona');
+
+        //Variáveis para acessar o valor do <input> a partir do name
+        var paciente = dadosPacientesFormulario(form);
+        var pacienteTr = criaTr(paciente);
+
+        if(!validarPaciente(paciente)){
+            console.log("Paciente Inválido!")
+            return;
+        }
+
+        var tabela = document.querySelector("#tabela-pacientes")
+        tabela.appendChild(pacienteTr);
+        form.reset();
+
+
+    }
+);
+
+function dadosPacientesFormulario(form){
+
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        IMC: calculaIMC(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+};
+
+function criaTr(paciente){
+      // cria o elemento <tr>
+      var pacienteTr = document.createElement("tr");
+      pacienteTr.classList.add("paciente");
+     
+      pacienteTr.appendChild(criaTd(paciente.nome, "info-nome"));
+      pacienteTr.appendChild(criaTd(paciente.peso, "info-nome"));
+      pacienteTr.appendChild(criaTd(paciente.altura, "info-nome"));
+      pacienteTr.appendChild(criaTd(paciente.gordura, "info-nome"));
+      pacienteTr.appendChild(criaTd(paciente.IMC, "info-nome"));
+
+      return pacienteTr;
+};
+
+function criaTd(dado, classes){
+
+    var td = document.createElement("td");
+    td.innerText = dado;
+    td.classList.add(classes);
+    return td;
+};
+
+function validarPaciente(){
+    if(validarPeso(paciente.peso)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
